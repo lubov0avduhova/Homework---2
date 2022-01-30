b0768fb583d37f67b35eb0e9ad7c8d0d5a0835ed
lubov0avduhova <loveavduhova@gmail.com>
2022-01-30 20:01:25 +0700

    первое задание завершено

--- ex.cs
+++ ex.cs
@@ -1,2 +1,20 @@
+using System;
 //1. Написать метод, возвращающий минимальное из трёх чисел.
 
+
+static void ex1
+{
+Console.WriteLine ("Впиши три числа: ");
+int first = int.Parse (Console.ReadLine());
+int second = int.Parse (Console.ReadLine());
+int third = int.Parse (Console.ReadLine());
+
+   if ( first > second && first > third) {
+
+      Console.WriteLine ($"Наибольшее число: {first}");
+
+   } else if (second > third) Console.WriteLine ($"Наибольшее число: {second}");
+   
+    else Console.WriteLine ($"Наибольшее число: {third}");
+
+}
