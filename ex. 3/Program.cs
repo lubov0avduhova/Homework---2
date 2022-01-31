d95cf1b1492074297ee181896e599721996cb709
lubov0avduhova <loveavduhova@gmail.com>
2022-02-01 06:54:00 +0700

    третье задание выполнено

--- ex. 3/Program.cs
+++ ex. 3/Program.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+
 /*
 3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
 */
-static void Main (){
 
-  System.Console.WriteLine();
+
+Console.WriteLine("Введи числа, пока не введешь ноль, программа не закроется");
+
+  int number = 0; 
+  int odd = 0; 
+do
+{
+    number = int.Parse (Console.ReadLine ());
+
+    if (number % 2 != 0)
+  {
+      odd = number + 1;
+  }
+
+} while (number != 0);{
+Console.WriteLine ("Число равно 0");
 }
+Console.WriteLine ($"Нечетные числа: {odd}");
+
