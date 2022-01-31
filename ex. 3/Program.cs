4853f1c3697c033d9919c66ab41ef5b03f837a6f
lubov0avduhova <loveavduhova@gmail.com>
2022-02-01 06:36:59 +0700

    изменения

--- ex. 3/Program.cs
+++ ex. 3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*
+3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+*/
+static void Main (){
+
+  System.Console.WriteLine();
+}
